3e6e9157f2ea990c6f1db1e8913325ff519dfca5
suzanaILIEVA <suzanaa.ilieva@gmail.com>
2024-08-29 16:35:15 +0200

    first

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {SafeAreaView, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text> deneme</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
